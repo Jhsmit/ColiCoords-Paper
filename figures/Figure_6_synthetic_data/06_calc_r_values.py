71a0c72ee5cb9d635efc0ca28ceb29d4e98a4e43
Jochem Smit <j.h.smit@rug.nl>
2019-02-08 20:13:10 +0100

    almost done

---
+++ figures/Figure_6_synthetic_data/06_calc_r_values.py
@@ -0,0 +1,75 @@
+import numpy as np
+from colicoords import load, save
+import os
+
+
+def get_r_vals(cell_obj):
+    r_i = cell_obj.coords.calc_rc(cell_obj.data.data_dict['storm_inner']['x'],
+                                  cell_obj.data.data_dict['storm_inner']['y'])
+    r_o = cell_obj.coords.calc_rc(cell_obj.data.data_dict['storm_outer']['x'],
+                                  cell_obj.data.data_dict['storm_outer']['y'])
+
+    return r_i, r_o
+
+
+def process_cells(m_cell, gt_cell):
+    """Measured cell and ground truth cell and returns r values for inner and outer membrane for measured and
+    ground truth, respectively"""
+    rc = gt_cell.coords.r / (1.5554007217841803 * 1.314602664567288)
+    r1 = get_r_vals(m_cell)
+    r2 = get_r_vals(gt_cell)
+
+    return list(r / rc for r in r1 + r2)
+
+
+def get_r_vals_all(data_dir):
+    gt_cells = load(os.path.join(data_dir, 'cell_obj', 'cells_final_selected.hdf5'))
+
+    for ph in [10000, 1000, 500]:
+        print('Photons', ph)
+        m_names = np.genfromtxt(os.path.join(data_dir, 'matched_names', 'm_cells_ph_{}_match_filter.txt'.format(ph)), dtype=str)
+        gt_names = np.genfromtxt(os.path.join(data_dir, 'matched_names', 'gt_cells_ph_{}_match_filter.txt'.format(ph)), dtype=str)
+
+        for condition in ['binary', 'brightfield', 'storm_inner']:
+            print('Condition', condition)
+            m_cells = load(os.path.join(data_dir, 'cell_obj', 'm_cells_ph_{}_filtered_{}.hdf5'.format(ph, condition)))
+
+            # Get index arrays to sort saved cell lists by matched names.
+            m_index = np.searchsorted(m_cells.name, m_names)
+            gt_index = np.searchsorted(gt_cells.name, gt_names)
+
+            # sorting CellList object by indexing; no copying is done.
+            m_sorted = m_cells[m_index]
+            gt_sorted = gt_cells[gt_index]
+
+            result = np.array([process_cells(m, gt) for m, gt in zip(m_sorted, gt_sorted)])
+
+            out_arr = np.full((len(m_sorted), 4, 720), fill_value=np.nan)  # Max number of localizations per cell < 720
+            for (r0, r1, r2, r3), elem in zip(result, out_arr):  # m_inner, m_outer, gt_inner, gt_outer
+                elem[0][:len(r0)] = r0
+                elem[1][:len(r1)] = r1
+                elem[2][:len(r2)] = r2
+                elem[3][:len(r3)] = r3
+
+            np.savetxt(os.path.join(data_dir, 'r_values', 'r_inner_m_ph_{}_{}.txt'.format(ph, condition)),
+                       out_arr[:, 0, :])
+            np.savetxt(os.path.join(data_dir, 'r_values', 'r_outer_m_ph_{}_{}.txt'.format(ph, condition)),
+                       out_arr[:, 1, :])
+            np.savetxt(os.path.join(data_dir, 'r_values', 'r_inner_gt_ph_{}_{}.txt'.format(ph, condition)),
+                       out_arr[:, 2, :])
+            np.savetxt(os.path.join(data_dir, 'r_values', 'r_outer_gt_ph_{}_{}.txt'.format(ph, condition)),
+                       out_arr[:, 3, :])
+
+            np.save(os.path.join(data_dir, 'r_values', 'r_inner_m_ph_{}_{}.npy'.format(ph, condition)),
+                    out_arr[:, 0, :])
+            np.save(os.path.join(data_dir, 'r_values', 'r_outer_m_ph_{}_{}.npy'.format(ph, condition)),
+                    out_arr[:, 1, :])
+            np.save(os.path.join(data_dir, 'r_values', 'r_inner_gt_ph_{}_{}.npy'.format(ph, condition)),
+                    out_arr[:, 2, :])
+            np.save(os.path.join(data_dir, 'r_values', 'r_outer_gt_ph_{}_{}.npy'.format(ph, condition)),
+                    out_arr[:, 3, :])
+
+
+if __name__ == '__main__':
+    data_dir = r'D:\Projects\CC_paper\figure_6_v3'
+    get_r_vals_all(data_dir)
---
+++ figures/Figure_6_synthetic_data/07a_calc_obj_values.py
@@ -0,0 +1,145 @@
+import numpy as np
+from colicoords import load, CellFit
+import os
+from tqdm.auto import tqdm
+
+
+#https://github.com/charnley/rmsd/blob/master/rmsd/calculate_rmsd.py
+def kabsch(P, Q):
+    # Computation of the covariance matrix
+    C = np.dot(np.transpose(P), Q)
+
+    # Computation of the optimal rotation matrix
+    # This can be done using singular value decomposition (SVD)
+    # Getting the sign of the det(V)*(W) to decide
+    # whether we need to correct our rotation matrix to ensure a
+    # right-handed coordinate system.
+    # And finally calculating the optimal rotation matrix U
+    # see http://en.wikipedia.org/wiki/Kabsch_algorithm
+    V, S, W = np.linalg.svd(C)
+    d = (np.linalg.det(V) * np.linalg.det(W)) < 0.0
+
+    if d:
+        S[-1] = -S[-1]
+        V[:, -1] = -V[:, -1]
+
+    # Create Rotation matrix U
+    U = np.dot(V, W)
+
+    return U
+
+
+def transform_coords(gt_cell, m_cell):
+    """uses kabsch to find the translation of storm localizations from m_cell onto gt_cell, then applies this transformation
+    to the midline polynomial and fits the result to a polynomial to obtain tranformed coords
+    """
+    P = np.stack([m_cell.data.data_dict['storm_inner']['x'], m_cell.data.data_dict['storm_inner']['y']]).T
+    Q = np.stack([gt_cell.data.data_dict['storm_inner']['x'], gt_cell.data.data_dict['storm_inner']['y']]).T
+
+    assert P.shape == Q.shape
+
+    x = np.linspace(m_cell.coords.xl, m_cell.coords.xr, num=1000, endpoint=True)
+    T = np.stack((x, m_cell.coords.p(x))).T
+
+    P_mean = np.mean(P, axis=0)
+    Q_mean = np.mean(Q, axis=0)
+
+    Pt = P - P_mean
+    Qt = Q - Q_mean
+
+    U = kabsch(Pt, Qt)
+
+    Tt = T - P_mean
+    Tr = np.dot(Tt, U)
+
+    Pr = np.dot(Pt, U)
+    Prt = Pr + Q_mean
+    Trt = Tr + Q_mean
+
+    xl = Trt.T[0].min()
+    xr = Trt.T[0].max()
+    r = m_cell.coords.r
+
+    a0, a1, a2 = np.polyfit(Trt.T[0], Trt.T[1], 2)[::-1]
+
+    d = {'a0': a0, 'a1': a1, 'a2': a2, 'xl': xl, 'xr': xr, 'r': r}
+
+    return d
+
+
+def cell_to_dict(cell):
+    return {attr: getattr(cell.coords, attr) for attr in ['a0', 'a1', 'a2', 'r', 'xl', 'xr']}
+
+
+def get_value(gt_cell, m_cell):
+    d_m = cell_to_dict(m_cell)
+    d_g = cell_to_dict(gt_cell)
+
+    d_g['r'] /= (1.5554007217841803 * 1.314602664567288)
+    d_m['r'] = d_g['r']
+
+    # copy the cell object because coords values get changed when calling the objective function
+    fit_gt = CellFit(gt_cell.copy(), 'storm_inner')
+    val_gt = fit_gt.fit.objective(**d_g)
+
+    fit_m = CellFit(m_cell.copy(), 'storm_inner')
+    val_m = fit_m.fit.objective(**d_m)
+
+    return val_m, val_gt
+
+
+def get_value_fit(gt_cell, m_cell):
+    d_m = cell_to_dict(m_cell)
+    d_g = cell_to_dict(gt_cell)
+
+    d_g['r'] /= (1.5554007217841803 * 1.314602664567288)
+    d_m['r'] = d_g['r']
+
+    # copy the cell object because coords values get changed when calling the objective function
+    fit_gt = CellFit(gt_cell.copy(), 'storm_inner')
+    val_gt = fit_gt.fit.objective(**d_g)
+
+    fit_m = CellFit(m_cell.copy(), 'storm_inner')
+    val_m = fit_m.fit.objective(**d_m)
+
+    res = fit_m.fit_parameters('r')
+    d_m['r'] = res.params['r']
+    val_m_new = fit_m.fit.objective(**d_m)
+
+
+    return val_m_new, val_gt
+
+def get_obj_values_all(data_dir):
+    gt_cells = load(os.path.join(data_dir, 'cell_obj', 'cells_final_selected.hdf5'))
+
+    for ph in [1000, 500]:
+        print('Photons', ph)
+        m_names = np.genfromtxt(os.path.join(data_dir, 'matched_names', 'm_cells_ph_{}_match_filter.txt'.format(ph)), dtype=str)
+        gt_names = np.genfromtxt(os.path.join(data_dir, 'matched_names', 'gt_cells_ph_{}_match_filter.txt'.format(ph)), dtype=str)
+
+        for condition in ['binary', 'brightfield', 'storm_inner']:
+            print('Condition', condition)
+            m_cells = load(os.path.join(data_dir, 'cell_obj', 'm_cells_ph_{}_filtered_{}.hdf5'.format(ph, condition)))
+
+            # Get index arrays to sort saved cell lists by matched names.
+            m_index = np.searchsorted(m_cells.name, m_names)
+            gt_index = np.searchsorted(gt_cells.name, gt_names)
+
+            # sorting CellList object by indexing; no copying is done.
+            m_sorted = m_cells[m_index]
+            gt_sorted = gt_cells[gt_index]
+
+            result = np.array([get_value(gt, m) for m, gt in tqdm(zip(m_sorted, gt_sorted), total=len(m_sorted))])
+
+            np.savetxt(os.path.join(data_dir, 'obj_values_new', 'obj_vals_storm_ph_{}_{}.txt'.format(ph, condition)), result)
+            np.save(os.path.join(data_dir, 'obj_values_new', 'obj_vals_storm_ph_{}_{}.npy'.format(ph, condition)), result)
+
+            # result = np.array([get_value_fit(gt, m) for m, gt in tqdm(zip(m_sorted, gt_sorted), total=len(m_sorted))])
+            #
+            # np.savetxt(os.path.join(data_dir, 'obj_values_new', 'obj_vals_fit_storm_ph_{}_{}.txt'.format(ph, condition)), result)
+            # np.save(os.path.join(data_dir, 'obj_values_new', 'obj_vals_fit_storm_ph_{}_{}.npy'.format(ph, condition)), result)
+
+
+if __name__ == '__main__':
+    data_dir = r'D:\Projects\CC_paper\figure_6_v3'
+    get_obj_values_all(data_dir)
