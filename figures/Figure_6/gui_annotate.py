b5ac1ffe88f57b89e429e72a22d4f38d5ff23a0f
J.H. Smit <jhsmit@gmail.com>
2019-04-10 12:57:18 +0200

    added gui annotate script

---
+++ figures/Figure_6/gui_annotate.py
@@ -0,0 +1,54 @@
+from colicoords.gui.controller import GenerateBinaryController
+from PyQt5 import QtGui
+import tifffile
+import os
+import sys
+import time
+import numpy as np
+
+"""
+Annotation of binary images requires the packages PyQt5 and pyqtgraph
+
+Controls:
+
+a:  Previous image
+d:  Next image
+e:  Increase brush size
+r:  Reduce brush size
+f:  Switch between paint / zoom mode
+
+Paint mode:
+Mouse 1: Draw overlay
+Mouse 2: Remove overlay
+
+Zoom mode:
+Mouse 1: Drag to pan
+Mouse 2: Access menu
+Mousewheel: Zoom in/out
+
+"""
+
+data_dir = r'.'
+
+bf_path = os.path.join(data_dir, 'Bf_corrected.tif')
+bf = tifffile.imread(bf_path)
+
+# To continue with a previously annotated file uncomment the next two lines and update the binary path
+#bin_path = 'BINARY_PATH'
+#binary = (tifffile.imread(bin_path) > 0).astype(int)
+
+binary = np.zeros_like(bf[:100])
+
+app = QtGui.QApplication(sys.argv)
+ctrl = GenerateBinaryController(bf[0:100], binary)
+
+
+# Use try/finally to ensure the result if saved even if the GUI crashes.
+try:
+    ctrl.show()
+    sys.exit(app.exec_())
+finally:
+    tifffile.imsave('binary_img_out_{}_asdf.tif'.format(int(time.time())), ctrl.output_binary)
+
+
+
